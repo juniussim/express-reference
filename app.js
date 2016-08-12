e10294690201591c0833384cece515616e718f22
Prima Aulia Gusta <primaulia@gmail.com>
2016-08-12 09:34:36 +0800

    latest express test

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var express = require('./config/express');
 var app = express();
 
 app.listen(7000);
-module.exports = app;
+module.exports = app; 
 
 console.log('My express server is running at localhost:7000');
