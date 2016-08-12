5ff5e41710f59d3f41caade076c39a13087df1b1
Prima Aulia Gusta <primaulia@gmail.com>
2016-08-12 14:42:51 +0800

    update my spec

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -20,7 +20,7 @@ describe("Express Server API", function() {
           .expect(200, done);
       });
 
-      it("returns a correct json format", function(done) {
+      xit("returns a correct json format", function(done) {
         supertest(app)
           .get('/users')
           .expect({
@@ -32,46 +32,44 @@ describe("Express Server API", function() {
   });
 });
 
+describe("Express Server Front end", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+      // request.get(base_url,
+      //   function(err, response, body) {
+      //     expect(response.statusCode).toBe(200);
+      //     expect(body).toBe("Hello world with MVC Structure");
+      //     done();
+      //   }
+      // );
 
+      supertest(app)
+      .get('/')
+      .set('Accept', 'text/html')
+      .expect('Content-Type', /html/)
+      .expect(200, done); // note that we're passing the done as parameter to the expect
+    });
+  });
 
-// describe("Express Server Front end", function() {
-//   describe("GET /", function() {
-//     it("returns status code 200", function(done) {
-//       // request.get(base_url,
-//       //   function(err, response, body) {
-//       //     expect(response.statusCode).toBe(200);
-//       //     expect(body).toBe("Hello world with MVC Structure");
-//       //     done();
-//       //   }
-//       // );
-//
-//       supertest(app)
-//       .get('/')
-//       .set('Accept', 'text/html')
-//       .expect('Content-Type', /html/)
-//       .expect(200, done); // note that we're passing the done as parameter to the expect
-//     });
-//   });
-//
-//   describe("GET /about", function() {
-//     it("returns status code 200", function(done) {
-//       request.get(base_url + 'about',
-//         function(err, response, body) {
-//           expect(response.statusCode).toBe(200);
-//           done();
-//         }
-//       );
-//     });
-//   });
-//
-//   describe("GET /contact", function() {
-//     it("returns status code 200", function(done) {
-//       request.get(base_url + 'contact',
-//         function(err, response, body) {
-//           expect(response.statusCode).toBe(200);
-//           done();
-//         }
-//       );
-//     });
-//   });
-// });
+  describe("GET /about", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url + 'about',
+        function(err, response, body) {
+          expect(response.statusCode).toBe(200);
+          done();
+        }
+      );
+    });
+  });
+
+  describe("GET /contact", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url + 'contact',
+        function(err, response, body) {
+          expect(response.statusCode).toBe(200);
+          done();
+        }
+      );
+    });
+  });
+});
