4db8519a1f14e54b3f42ff799c8061e190720028
Prima Aulia Gusta <primaulia@gmail.com>
2016-08-12 09:47:33 +0800

    exports the app for test purposes

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ app.set('port', (process.env.PORT || 7000));
 app.listen(app.get('port'), function() {
   console.log('My express server is running at localhost', app.get('port'));
 });
+
+module.exports = app;
