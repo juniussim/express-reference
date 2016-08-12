fe7c43b667acf1c7add56951e13b6e6108ed9625
Prima Aulia Gusta <primaulia@gmail.com>
2016-08-12 09:43:00 +0800

    set up app.js to work with heroku

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 var express = require('./config/express');
 var app = express();
 
-app.listen(7000);
-module.exports = app; 
+app.set('port', (process.env.PORT || 7000));
 
-console.log('My express server is running at localhost:7000');
+app.listen(app.get('port'), function() {
+  console.log('My express server is running at localhost', app.get('port'));
+});
